001520958c8fff44642f9b6099259e29dd164d83
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-06-18 01:23:45 -0300

    add Team component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,47 @@ import { useState } from "react";
 
 import Banner from "./components/Banner/banner.jsx";
 import Form from "./components/Form/form.jsx";
+import Team from "./components/Team/team.jsx";
 
 function App() {
+  const teams = [
+    {
+      name: "Programação",
+      primaryColor: "#57C278",
+      secondaryColor: "#D9F7E9",
+    },
+    {
+      name: "Front-End",
+      primaryColor: "#82CFFA",
+      secondaryColor: "#E8F8FF",
+    },
+    {
+      name: "Data Science",
+      primaryColor: "#A6D157",
+      secondaryColor: "#F0F8E2",
+    },
+    {
+      name: "Devops",
+      primaryColor: "#E06B69",
+      secondaryColor: "#FDE7E8",
+    },
+    {
+      name: "UX e Design",
+      primaryColor: "#D86EBF",
+      secondaryColor: "#FAE5F5",
+    },
+    {
+      name: "Mobile",
+      primaryColor: "#FEBA05",
+      secondaryColor: "#FFF5D9",
+    },
+    {
+      name: "Inovação e Gestão",
+      primaryColor: "#FF8A29",
+      secondaryColor: "#FFEEDF",
+    },
+  ];
+
   const [collaborators, setCollaborators] = useState([]);
 
   const handleAddCollaborator = (collaborator) => {
@@ -15,6 +54,16 @@ function App() {
     <>
       <Banner />
       <Form onAddCollaborator={handleAddCollaborator} />
+      {teams.map((team) => {
+        return (
+          <Team
+            key={team.name}
+            name={team.name}
+            primaryColor={team.primaryColor}
+            secondaryColor={team.secondaryColor}
+          />
+        );
+      })}
     </>
   );
 }
